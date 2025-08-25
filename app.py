55e4dcb720d04adf3325af1c130c2ca91d2448ab
gedsonaltoe11 <gedson.altoe11@gmail.com>
2025-08-25 09:07:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 app = FastAPI()
 
 # Pegando o email da variável de ambiente
-UNPAYWALL_EMAIL = os.getenv("UNPAYWALL_EMAIL")
+UNPAYWALL_EMAIL = os.getenv("Gedson.altoe11@gmail.com")
 
 @app.get("/")
 def raiz():
